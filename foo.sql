1a915932960ded62376dd7a8eebca2bb02d9447f
Chase Tillar <35161303+unignostik@users.noreply.github.com>
2018-07-29 19:42:08 -0500

    Create foo.sql

---
+++ foo.sql
@@ -0,0 +1,3 @@
+LEFT(SUBSTRING(Result, (CHARINDEX(‘caFirstname’,Result, 0)+14, 20), LEN(SUBSTRING(Result, (CHARINDEX(‘caFirstname’,Result, 0)+14, 20)-1),
+LEFT(SUBSTRING(Result, (CHARINDEX(‘caLastname’,Result, 0)+14, 20), LEN(SUBSTRING(Result, (CHARINDEX(‘caLastname’,Result, 0)+14, 20)-1),
+LEFT(SUBSTRING(Result, (CHARINDEX(‘caEmail’,Result, 0)+14, 20), LEN(SUBSTRING(Result, (CHARINDEX(‘caEmail’,Result, 0)+14, 20)-1)
