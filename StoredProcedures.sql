0ef69474eca47d0bf3c8ce1b0c24decf2c5d081d
Chase Tillar <35161303+unignostik@users.noreply.github.com>
2018-07-29 20:52:01 -0500

    Update StoredProcedures.sql

--- StoredProcedures.sql
+++ StoredProcedures.sql
@@ -3,8 +3,8 @@ LANGUAGE SQL
 P1: BEGIN
 
 SET LEFT(SUBSTRING(Result, (CHARINDEX(‘caFirstname’,Result, 0)+14, 20), LEN(SUBSTRING(Result, (CHARINDEX(‘caFirstname’,Result, 0)+14, 20)-1) = firstName;
-SET LEFT(SUBSTRING(Result, (CHARINDEX(‘caLastname’,Result, 0)+14, 20), LEN(SUBSTRING(Result, (CHARINDEX(‘caLastname’,Result, 0)+14, 20)-1) = lastName;
-SET LEFT(SUBSTRING(Result, (CHARINDEX(‘caEmail’,Result, 0)+14, 20), LEN(SUBSTRING(Result, (CHARINDEX(‘caEmail’,Result, 0)+14, 20)-1) = email;
+SET LEFT(SUBSTRING(Result, (CHARINDEX(‘caLastname’,Result, 0)+13, 20), LEN(SUBSTRING(Result, (CHARINDEX(‘caLastname’,Result, 0)+13, 20)-1) = lastName;
+SET LEFT(SUBSTRING(Result, (CHARINDEX(‘caEmail’,Result, 0)+10, 20), LEN(SUBSTRING(Result, (CHARINDEX(‘caEmail’,Result, 0)+10, 20)-1) = email;
                                                                         
 FROM db2_audit
 WHERE CompanyName = company
