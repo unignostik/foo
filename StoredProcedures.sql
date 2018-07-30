5cda985e750c08a54ebe75b4a91e3f5a54bd4648
Chase Tillar <35161303+unignostik@users.noreply.github.com>
2018-07-29 20:48:48 -0500

    Update StoredProcedures.sql

--- StoredProcedures.sql
+++ StoredProcedures.sql
@@ -1,1 +1,13 @@
 CREATE PROCEDURE updateAPA (IN company VARCHAR(100), IN firstName VARCHAR(100), IN lastName VARCHAR(100), IN email VARCHAR(100)
+LANGUAGE SQL
+P1: BEGIN
+
+SET LEFT(SUBSTRING(Result, (CHARINDEX(‘caFirstname’,Result, 0)+14, 20), LEN(SUBSTRING(Result, (CHARINDEX(‘caFirstname’,Result, 0)+14, 20)-1) = firstName;
+SET LEFT(SUBSTRING(Result, (CHARINDEX(‘caLastname’,Result, 0)+14, 20), LEN(SUBSTRING(Result, (CHARINDEX(‘caLastname’,Result, 0)+14, 20)-1) = lastName;
+SET LEFT(SUBSTRING(Result, (CHARINDEX(‘caEmail’,Result, 0)+14, 20), LEN(SUBSTRING(Result, (CHARINDEX(‘caEmail’,Result, 0)+14, 20)-1) = email;
+                                                                        
+FROM db2_audit
+WHERE CompanyName = company
+                                                                        
+END P1                                                                       
+                            
